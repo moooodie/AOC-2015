8f1725b06b2fb856916aaa9bccd97e0e6587d0b4
justin <skulllit@gmail.com>
2026-02-04 21:22:04 +0100

    day 8 done

---
+++ day8.fsx
@@ -0,0 +1,38 @@
+let input = 
+    System.IO.File.ReadAllLines "input.txt"
+
+let partOne = 
+    input
+    |> Array.sumBy (fun str ->
+        let inMemoryCount = 
+            str
+            |> Seq.fold (fun (escapeCount, count) letter ->
+                match escapeCount, letter with
+                | 0, '\\' -> 1, count + 1
+                | 0, l -> 0, count + 1
+                | 1, ('"' | '\\') -> 0, count
+                | 1, 'x' -> 2, count
+                | 1, l -> failwith "can only have x, \\ or \" after first slash"
+
+                | num, l ->
+                    if num = 3 then
+                        0, count
+                    else num + 1, count
+
+                ) (0, 0)
+            |> fun (_, c) -> c - 2
+
+        str.Length - inMemoryCount)
+
+
+let partTwo = 
+    input
+    |> Array.sumBy (fun str ->
+        let requiredLiteralStringLength = 
+           str
+           |> Seq.sumBy(
+                function
+                | '"' | '\\' -> 2
+                | _ -> 1)
+
+        2 + requiredLiteralStringLength - str.Length)
